f027b0f9b4a9a05559b8bb035275a3ce654b17bf
ranulfjn <ranulfnoronha@gmail.com>
2021-03-29 19:19:43 -0400

    components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,33 @@
 import React from 'react'
+import memories from './assets/memories.png'
+import { Container , AppBar , Grid , Typography ,Grow} from '@material-ui/core'
 
-const app = () => {
+const App = () => {
     return (
-        <div>
+        <>
+        <Container maxWidth ="lg">
+            <AppBar position="static" color="inherit">
+                <Typography varient="h2" align ="center">Memories</Typography>
+                <img src={memories} alt='memories' height='60'></img>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify='space-between' alighItems='stretch' spacing={4} >
+                        <Grid item xs ={12} sm={7} >
+                            <Posts />
+                        </Grid>
+                        <Grid item xs ={12} sm={4} >
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+
+            </Grow>
+
+        </Container>
             
-        </div>
+        </>
     )
 }
 
-export default app;
+export default App;
